2cce815b0570490b7299dc142f2f95504f979e3a
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-17 21:08:22 +0500

    add mongoose

--- src/controllers/auth/password-recovery.controller.ts
+++ src/controllers/auth/password-recovery.controller.ts
@@ -1,6 +1,16 @@
 import {Request, Response} from 'express';
 import {authService} from "../../domain/auth/auth.service";
+import {HTTP_STATUSES} from "../../models/common/common.types";
+import {ErrorAuth} from "../../models/auth/ouput/auth.service.models";
 
 export const passwordRecoveryController = async (req: Request, res: Response) => {
     const sendSMS = await authService.passwordRecovery(req.body.email);
+    if (sendSMS instanceof ErrorAuth || sendSMS.data === null){
+        res
+            .sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
+        return;
+    }
+    res
+        .sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+    return;
 }
