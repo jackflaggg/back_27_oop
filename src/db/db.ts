64efd0644cad674e14787198ed3d8168b2fd9832
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-17 16:59:33 +0500

    add mongoose

--- src/db/db.ts
+++ src/db/db.ts
@@ -14,19 +14,81 @@ const client: MongoClient = new MongoClient(mongoURI!);
 
 export const database: Db = client.db(SETTINGS.DB_NAME);
 
-export const blogsCollections = database.collection<BlogDbType>(SETTINGS.COLLECTION_BLOGS!);
-export const postsCollections = database.collection<PostDbType>(SETTINGS.COLLECTION_POSTS!);
-export const usersCollection = database.collection<UserDbType>(SETTINGS.COLLECTION_USERS!);
-export const commentsCollection = database.collection<CommentDbType>(SETTINGS.COLLECTION_COMMENTS!);
-export const refreshTokenCollection = database.collection<RefreshTokenType>(SETTINGS.COLLECTION_TOKEN!)
-export const sessionCollection = database.collection<SessionCollection>(SETTINGS.COLLECTION_API!);
+const blogSchema = new mongoose.Schema({
+    // id?:            String,
+    name:           String,
+    description:    String,
+    websiteUrl:     String,
+    createdAt:      String,
+    isMembership:   Boolean,
+});
+
+const postSchema = new mongoose.Schema({
+    title:              String,
+    shortDescription:   String,
+    content:            String,
+    blogId:             String,
+    blogName:           String,
+    createdAt:          String,
+});
+
+const userSchema = new mongoose.Schema({
+        login:                  String,
+        password:               String,
+        email:                  String,
+        createdAt:              String,
+        emailConfirmation:      {
+            confirmationCode: { type: String, required: false },
+            // меняется на null
+            expirationDate: { type: Date },
+            isConfirmed: { type: Boolean, required: true, default: false }
+        }
+});
+
+const commentSchema = new mongoose.Schema({
+    content:            String,
+    commentatorInfo:    {
+        userId:         String,
+        userLogin:      String
+    },
+    createdAt:          String,
+    postId:             String,
+});
+
+const refreshSchema = new mongoose.Schema({
+    refreshToken:   String
+});
+
+const sessionSchema = new mongoose.Schema({
+    issuedAt:           String,
+    deviceId:           String,
+    userId:             String,
+    ip:                 String,
+    lastActiveDate:     String,
+    deviceName:         String,
+    refreshToken:       String,
+});
+
+const RecoveryPasswordSchema = new mongoose.Schema({
+    userId: String,
+    recoveryCode: String,
+    expirationDate: Date
+});
+
+// export const blogsCollections = database.collection<BlogDbType>(SETTINGS.COLLECTION_BLOGS!);
+// export const postsCollections = database.collection<PostDbType>(SETTINGS.COLLECTION_POSTS!);
+// export const usersCollection = database.collection<UserDbType>(SETTINGS.COLLECTION_USERS!);
+// export const commentsCollection = database.collection<CommentDbType>(SETTINGS.COLLECTION_COMMENTS!);
+// export const refreshTokenCollection = database.collection<RefreshTokenType>(SETTINGS.COLLECTION_TOKEN!)
+// export const sessionCollection = database.collection<SessionCollection>(SETTINGS.COLLECTION_API!);
 
 export const connectToDB = async (port: number) => {
     try {
-        await client.connect()
+        await mongoose.connect(mongoURI!)
         console.log('connected to db')
     } catch (err) {
-        console.error('Failed to connect to DB', err);
+        console.log('Failed to connect to DB', String(err));
+        await mongoose.disconnect();
         process.exit(1);
     }
 }
