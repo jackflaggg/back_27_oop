1ee5c6dac7e1c32d9443b7a41ed31d2675219c49
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-20 00:03:07 +0500

    отключил версионирование ключа

--- src/utils/mappers/device.mapper.ts
+++ src/utils/mappers/device.mapper.ts
@@ -1,16 +1,5 @@
-import {InDeviceSession} from "../../models/devices/input/create.device.session.model";
 import {WithId} from "mongodb";
 import {SessionCollection} from "../../models/db/db.models";
-
-export const deviceMapper = (model: InDeviceSession) => {
-    return {
-        deviceId: model.deviceId,
-        userId: model.userId,
-        ip: model.ip,
-        title: model.deviceName,
-        refreshToken: model.refreshToken,
-    }
-}
 
 export const outDeviceMapper = (model: WithId<SessionCollection>) => {
     return {
