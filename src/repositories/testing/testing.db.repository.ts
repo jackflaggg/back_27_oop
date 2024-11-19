8b9a96f96711575b594aaf32456e9f19708fbbd9
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-19 23:18:27 +0500

    отключил версионирование ключа

--- src/repositories/testing/testing.db.repository.ts
+++ src/repositories/testing/testing.db.repository.ts
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 export const testingDbRepositories = {
     async deleteAllData(): Promise<void>{
         try {
-            const currentConnection = mongoose.connection;
-            console.log('текущее соединение: ', currentConnection);
-            await currentConnection.dropDatabase();
+            await mongoose.connection.dropDatabase();
             console.log('база данных очищена!')
         } catch(err: unknown){
             console.log('Failed to delete all data', String(err))
