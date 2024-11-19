858be5b96211b7026bc7d11294ae754b1d2fb2d2
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-19 22:10:49 +0500

    добавил оптимистичный параллелизм

--- src/managers/email.managers.ts
+++ src/managers/email.managers.ts
@@ -1,7 +1,7 @@
 import {emailAdapter} from "../utils/adapters/email.adapter";
 
 export const emailManagers = {
-    async sendEmailRecoveryMessage(email: string,  confirmationCode: string, operationID: string): Promise<void> {
+    async sendEmailRecoveryMessage(email: string,  confirmationCode: string, operationID: string) {
         return await emailAdapter.sendEmail(email, confirmationCode, operationID);
     }
 }
