3976e3b698ac6bbd100a54a81863d31cb3c48d55
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-23 20:21:21 +0500

    add hash-service

--- src/routes/blogs/blog.router.ts
+++ src/routes/blogs/blog.router.ts
@@ -9,13 +9,13 @@ export class BlogRouter extends BaseRouter {
     constructor( logger: LoggerService, private blogsQueryRepo: BlogsQueryRepositories) {
         super(logger);
         this.bindRoutes([
-            { path: '/',            method: 'get', func: this.getAllBlogs},
-            { path: '/:id',         method: 'get', func: this.getOneBlog},
-            { path: '/:id/posts',   method: 'get', func: this.getAllPostsToBlog},
-            { path: '/',            method: 'post', func: this.createBlog},
-            { path: '/:id/posts',   method: 'post', func: this.createPostToBlog},
-            { path: '/:id',         method: 'put', func: this.updateBlog},
-            { path: '/:id',         method: 'delete', func: this.deleteBlog},
+            { path: '/',            method: 'get',      func: this.getAllBlogs},
+            { path: '/:id',         method: 'get',      func: this.getOneBlog},
+            { path: '/:id/posts',   method: 'get',      func: this.getAllPostsToBlog},
+            { path: '/',            method: 'post',     func: this.createBlog},
+            { path: '/:id/posts',   method: 'post',     func: this.createPostToBlog},
+            { path: '/:id',         method: 'put',      func: this.updateBlog},
+            { path: '/:id',         method: 'delete',   func: this.deleteBlog},
             ])
     }
     async getAllBlogs(req: RequestWithQuery<QueryBlogInputInterface>, res: Response, next: NextFunction){
