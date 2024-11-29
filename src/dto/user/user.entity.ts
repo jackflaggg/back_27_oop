4f0da6b04162c1d09a82dd4db74a5406104903ad
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-29 15:23:52 +0500

    add hash-service

--- src/dto/user/user.entity.ts
+++ src/dto/user/user.entity.ts
@@ -1,6 +1,34 @@
+import {compare, genSalt, hash} from "bcrypt";
+
 export class User {
-    createdAt: Date;
-    constructor(protected login: string, protected password: string, protected email: string) {
-        this.createdAt = new Date();
+    private _createdAt: Date;
+    private _password: string | undefined;
+
+    constructor(private readonly _login: string, private readonly _email: string, passwordHash?: string) {
+        if (passwordHash){
+            this._password = passwordHash;
+        }
+        this._createdAt = new Date();
+    }
+
+    get email(): string{
+        return this._email;
+    }
+
+    get login(): string{
+        return this._login;
+    }
+
+    get password(): string {
+        return String(this._password);
+    }
+
+    public async setPassword(password: string, salt: number): Promise<void> {
+        const saltRound = await genSalt(salt);
+        this._password = await hash(password, saltRound);
+    }
+
+    public async comparePassword(pass: string, hash: string): Promise<boolean> {
+        return await compare(pass, hash);
     }
 }
