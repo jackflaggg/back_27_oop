d532448d25641c5a17d60cb282c347eedc6ac731
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-30 12:45:03 +0500

    add auth service

--- src/domain/auth/auth.service.ts
+++ src/domain/auth/auth.service.ts
@@ -1,12 +1,31 @@
 import {UserCreateDto} from "../../dto/user/user.create.dto";
 import {User} from "../../dto/user/user.entity";
 import {SETTINGS} from "../../settings";
+import {emailManagers} from "../../managers/email.manager";
+import {UsersDbRepository} from "../../repositories/users/users.db.repository";
 
 export class AuthService {
+    constructor(private readonly userDbRepository: UsersDbRepository){}
     async registrationUser(userDto: UserCreateDto) {
         const user = new User(userDto.login, userDto.email);
 
         await user.setPassword(userDto.password, SETTINGS.SALT);
 
+        const newUser = user.mappingUserCreateClient();
+
+        const createUser = await this.userDbRepository.createUser(newUser);
+
+        const {email: userEmail, emailConfirmation: {confirmationCode}} = newUser;
+
+        emailManagers.sendEmailRecoveryMessage(userEmail, confirmationCode)
+            .then(async (existingSendEmail) => {
+                if (!existingSendEmail) {
+                    await this.userDbRepository.deleteUser(String(createUser));
+                }
+            })
+            .catch(async (e) => {
+                console.log(String(e))
+                await this.userDbRepository.deleteUser(String(createUser));
+            })
     }
 }
