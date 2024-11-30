c040e3f905057267b1ae0723b64d87a6780e5268
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-30 12:30:50 +0500

    add hash-service

--- src/middlewares/limiter.middleware.ts
+++ src/middlewares/limiter.middleware.ts
@@ -4,14 +4,17 @@ import {IMiddleware} from "../routes/route.interface";
 import {Response, Request, NextFunction, Router} from "express";
 
 export class Limiter implements IMiddleware {
-    async execute(req: Request, res: Response, next: NextFunction) {
-        rateLimit({
+    private _limiter;
+    constructor() {
+        this._limiter = rateLimit({
             windowMs: 10 * 1000,
             limit: 5,
             message: 'слишком много запросов, повтори попытку через 10 секунд',
             statusCode: HTTP_STATUSES.TOO_MANY_REQUESTS_429
         })
-        next()
+    }
+    async execute(req: Request, res: Response, next: NextFunction) {
+        this._limiter(req, res, next);
     }
 }
 export const registrationLimiter = rateLimit({
