c8ee0d0220b11086daa6f5b22cf64b199318755d
jackflaggg <rasul.khamzinnn@gmail.com>
2024-12-04 23:45:21 +0500

    add auth service

--- src/repositories/comments/comments.query.repository.ts
+++ src/repositories/comments/comments.query.repository.ts
@@ -1,6 +1,7 @@
 import {transformComment} from "../../utils/features/mappers/comment.mapper";
 import {CommentModelClass} from "../../db/db";
 import {ObjectId} from "mongodb";
+import {queryHelperToPost} from "../../utils/features/query/query.helper";
 
 export class CommentsQueryRepository {
     async getComment(idComment: string) {
@@ -13,6 +14,25 @@ export class CommentsQueryRepository {
         return transformComment(comment);
     }
     async getAllCommentsToPostId(paramsToPostId: string, queryComments: any) {
+        const {pageNumber, pageSize, sortBy, sortDirection} = queryHelperToPost(queryComments);
 
+        const comments = await CommentModelClass
+            .find({postId: paramsToPostId})
+            .sort({[sortBy]: sortDirection === 'asc' ? 1 : -1})
+            .skip((Number(pageNumber) - 1) * Number(pageSize))
+            .limit(Number(pageSize))
+            .lean();
+
+        const totalCountComments = await CommentModelClass.countDocuments({postId: paramsToPostId});
+
+        const pagesCount = Math.ceil(totalCountComments / Number(pageSize));
+
+        return {
+            pagesCount: +pagesCount,
+            page: +pageNumber,
+            pageSize: +pageSize,
+            totalCount: +totalCountComments,
+            items: comments.map(comments => transformComment(comments))
+        }
     }
 }
