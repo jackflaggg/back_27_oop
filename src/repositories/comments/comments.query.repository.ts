07dbc3db64cc2d2a9d05c7a418f8d869c70b70f5
jackflaggg <rasul.khamzinnn@gmail.com>
2024-12-04 23:45:01 +0500

    add auth service

--- src/repositories/comments/comments.query.repository.ts
+++ src/repositories/comments/comments.query.repository.ts
@@ -1,6 +1,16 @@
+import {transformComment} from "../../utils/features/mappers/comment.mapper";
+import {CommentModelClass} from "../../db/db";
+import {ObjectId} from "mongodb";
+
 export class CommentsQueryRepository {
     async getComment(idComment: string) {
+        const comment = await CommentModelClass.findOne({ _id: new ObjectId(idComment)});
 
+        if (!comment) {
+            return;
+        }
+
+        return transformComment(comment);
     }
     async getAllCommentsToPostId(paramsToPostId: string, queryComments: any) {
 
